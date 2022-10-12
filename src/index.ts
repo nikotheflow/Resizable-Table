fe0e464e2a5148ae2e8fc22f989ec095e65dc118
Иван Никитенко <nikotheflow@yandex.ru>
2022-10-12 10:42:57 +0300

    Add functions export

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,5 @@
 import { initResizableTable } from "./ts/resizable-columns";
 import { initTableWithToggleableColumns } from './ts/toggleable-columns';
-
-// const tables: HTMLTableElement[] = Array.from(document.querySelectorAll('.table-resizable'));
-
-// tables.forEach(table => {
-// 	initResizableTable(table);
-// 	initTableWithToggleableColumns(table);
-// });
-
 import { initCustomTable } from "./ts/custom-table";
 
-const tables: HTMLTableElement[] = Array.from(document.querySelectorAll('table'));
-
-for (const table of tables) {
-	initCustomTable(table);
-}
+module.exports = {initResizableTable, initTableWithToggleableColumns, initCustomTable};
